8ad039115756528143dedb27d976babc75d9eb13
Leonid Filippov <filipleo@cvut.cz>
2022-10-06 19:12:04 +0200

    Added messages for competitions and code

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+from cgitb import text
+import logging
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
@@ -5,6 +8,12 @@ from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
 from telegram.ext.filters import Filters
 from credentials import bot_token
+from config import BUTTONS, INFO
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 updater = Updater(bot_token,
 				use_context=True)
@@ -12,27 +21,29 @@ updater = Updater(bot_token,
 
 def start(update: Update, context: CallbackContext):
 	update.message.reply_text(
-		"Hello sir, Welcome to the Bot.Please write\
+		"Hello sir, Welcome to the mitno bot. Please write\
 		/help to see the commands available.")
+	buttons = [[KeyboardButton(BUTTONS["competition"])], [KeyboardButton(BUTTONS["code"])]]
+	context.bot.send_message(chat_id=update.effective_chat.id, text="Hello", reply_markup=ReplyKeyboardMarkup(buttons))
+
+def message_handler(update: Update, context: CallbackContext):
+	if BUTTONS["competition"] in update.message.text:
+		update.message.reply_text(INFO["competition_info"])
+	if BUTTONS["code"] in update.message.text:
+		update.message.reply_text(INFO["code_info"])
 
 def help(update: Update, context: CallbackContext):
-	update.message.reply_text("""The list of commands (and buttons in the future)""")
+	update.message.reply_text("Egor is very cool men (it isn't joke)")
 
 def unknown_text(update: Update, context: CallbackContext):
-	update.message.reply_text(
-		"Sorry I can't recognize you , you said '%s'" % update.message.text)
+	update.message.reply_text(f"Sorry I can't recognize you , you said {update.message.text}")
 
 def unknown(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Sorry '%s' is not a valid command" % update.message.text)
+    update.message.reply_text(f"Sorry {update.message.text} is not a valid command")
 
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
-updater.dispatcher.add_handler(MessageHandler(
-	Filters.command, unknown))
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 
 updater.start_polling()
---
+++ config.py
@@ -0,0 +1,33 @@
+BUTTONS = {
+    "competition": "I have won the Minto competition",
+    "code": "I have a code from Minto card",
+    "valid_data": "The data is correct",
+    "not_valid_data": "There is something wrong with the data",
+}
+
+INFO = {
+    "competition_info": """
+Congratulations!
+
+Please send us following data:
+— a link to the competition you participated in
+—  your nickname from the social media where you won
+— your wallet number on BSC chain
+    """,
+    "code_info": """
+Cool that you decided to try mining with Minto! We need quite a bit of data to send you tokens.
+In the booklet, on the card under the erasable layer, you should have a code. Please send it along with your wallet number on BSC chain.
+    """,
+    "info_answ": "Thanks! Give me up to one hour to check the data.",
+    "failed_answ": """
+        Sorry, it seems that your data is not accurate and we couldn’t verify it.
+        If you want more details or have any questions — we can kindly connect you with our support team
+    """,
+    "correct_answ": """
+        Cool! Tokens are already on your wallet. 
+        All you need to do is stake them to receive an everyday reward in BTC. Here are detailed instructions on how to do it.
+        To receive a new info about the project please follow us on:
+        telegram | discord |  twitter
+        Do you have any questions?
+    """,
+}
